cdec5ae747fa68793ee144a6f158eb7d87600be4
wsr10 <wsr10@K218PC11>
2022-04-11 11:53:07 +0300

    Добавьте файлы проекта.

---
+++ Delegat7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Delegat7
+
+{
+    public delegate bool CompareDelegate(int a, int b);
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CompareDelegate cD = (x, y) => x == y;
+            bool result = cD(9, 10);
+            Console.WriteLine(result.ToString());
+            Console.ReadKey();
+        }
+    }
+}
